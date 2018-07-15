6a12e0f5c30d8b76f6bdd8edefeb0b76b54114a5
Jesse <jsoldat@hotmail.com>
2018-07-15 10:33:47 +0700

    refactor intial app start up

--- client/src/index.js
+++ client/src/index.js
@@ -5,20 +5,12 @@ import registerServiceWorker from "./registerServiceWorker";
 
 // css
 import "./index.css";
-
 // router
 import AppRouter from "./router/AppRouter";
 // store
 import configureStore from "./store/configureStore";
-
-// actions & action helpers
-import buildClientMsg from "./actions/helpers/buildClientMsg";
-import { AUTH_LOGIN } from "./actions/auth";
-import { NEW_MSG } from "./actions/ui";
-// utils
-import setAxiosHeader from "./utils/setAxiosHeader";
-import isTokenExp from "./utils/isTokenExp";
-import deleteExpToken from "./utils/deleteExpToken";
+// utils - auth
+import handleInitialAuth from "./utils/auth/handleInitialAuth";
 
 const Loading = () => "loading..";
 const store = configureStore();
@@ -41,36 +33,4 @@ const renderApp = () => {
 ReactDOM.render(<Loading />, document.getElementById("root"));
 registerServiceWorker();
 
-const user = JSON.parse(localStorage.getItem("user"));
-
-if (user) {
-  const { _id, token } = user;
-
-  if (isTokenExp(token)) {
-    localStorage.removeItem("user");
-    setAxiosHeader(null);
-    store.dispatch({
-      type: NEW_MSG,
-      msg: buildClientMsg({
-        info: "Your session expired please login again.",
-        color: "blue"
-      }),
-      loading: false
-    });
-
-    deleteExpToken(token);
-
-    renderApp();
-  } else {
-    store.dispatch({
-      type: AUTH_LOGIN,
-      _id,
-      token
-    });
-    setAxiosHeader(token);
-    renderApp();
-  }
-} else {
-  setAxiosHeader(null);
-  renderApp();
-}
+handleInitialAuth(store, renderApp);
